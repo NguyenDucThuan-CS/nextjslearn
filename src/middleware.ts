ae045934ff09fe6ec0d36ade1321754afa69b8ae
Nguyen Duc Thuan <ndthuan2@ppj-international.com>
2024-10-27 17:43:33 +0700

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+
+const protectedRoutes = ['/me'];
+const authRoutes = ['/login', '/register'];
+export function middleware(request: NextRequest) {
+  const sessionToken = request.cookies.get('sessionToken')?.value;
+  console.log('sessionToken', sessionToken)
+  if(protectedRoutes.includes(request.nextUrl.pathname) && !sessionToken) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+  if(authRoutes.includes(request.nextUrl.pathname) && sessionToken) {
+    return NextResponse.redirect(new URL('/me', request.url))
+  }
+  return NextResponse.next()
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    ...protectedRoutes,
+    ...authRoutes
+  ],
+}
